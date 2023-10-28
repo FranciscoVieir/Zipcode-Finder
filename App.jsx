47b250f875827f0780d17367f8c02ad802f3237d
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-10-28 10:18:49 -0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
