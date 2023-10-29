74cd06f685474f5134d6d7051afd9380bbb6370e
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-10-29 09:31:43 -0300

    feat: adding the feature to search the zip code in the api and improving the interface design

--- App.jsx
+++ App.jsx
@@ -1,5 +1,4 @@
-import React, {useState} from 'react';
-
+import React, {useRef, useState} from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,41 +6,76 @@ import {
   TouchableOpacity,
   SafeAreaView,
   TextInput,
+  Keyboard,
 } from 'react-native';
 
 import api from './src/services/api';
 
 function App() {
   const [cep, setCep] = useState('');
+  const inputRef = useRef(null);
+  const [userCep, setUserCep] = useState(null);
+
+  function toClean() {
+    setCep('');
+    inputRef.current.focus();
+    setUserCep(null);
+  }
+
+  async function toSearch() {
+    if (cep === '') {
+      alert('Digite um CEP válido');
+      setCep('');
+      return;
+    }
+
+    try {
+      const response = await api.get(`/${cep}/json`);
+      console.log(response.data);
+
+      setUserCep(response.data);
+
+      Keyboard.dismiss();
+    } catch (error) {
+      console.log('Erro: ' + error);
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{alignItems: 'center'}}>
-        <Text style={styles.text}>Digite o cep desejado:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Ex: 79003242"
-          value={cep}
-          onChangeText={text => setCep(text)}
-          keyboardType="numeric"
-        />
-      </View>
+      <Text style={styles.title}>Digite o CEP desejado:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Ex: 79003242"
+        value={cep}
+        onChangeText={text => setCep(text)}
+        keyboardType="numeric"
+        ref={inputRef}
+      />
       <View style={styles.buttonArea}>
-        <TouchableOpacity style={[styles.button, {backgroundColor: '#1d75cd'}]}>
+        <TouchableOpacity
+          style={[styles.button, styles.searchButton]}
+          onPress={toSearch}>
           <Text style={styles.buttonText}>Buscar</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, {backgroundColor: '#cd5e1d'}]}>
+        <TouchableOpacity
+          style={[styles.button, styles.clearButton]}
+          onPress={toClean}>
           <Text style={styles.buttonText}>Limpar</Text>
         </TouchableOpacity>
       </View>
 
-      <View style={styles.result}>
-        <Text style={styles.itemText}>CEP: 93029302</Text>
-        <Text style={styles.itemText}>Logradouro: Rua dos devs</Text>
-        <Text style={styles.itemText}>Bairro: Park</Text>
-        <Text style={styles.itemText}>Cidade: Fortaleza</Text>
-        <Text style={styles.itemText}>Estado: CE</Text>
-      </View>
+      {userCep && (
+        <View style={styles.result}>
+          <Text style={styles.resultText}>CEP: {userCep.cep}</Text>
+          <Text style={styles.resultText}>
+            Logradouro: {userCep.logradouro}
+          </Text>
+          <Text style={styles.resultText}>Bairro: {userCep.bairro}</Text>
+          <Text style={styles.resultText}>Cidade: {userCep.localidade}</Text>
+          <Text style={styles.resultText}>Estado: {userCep.uf}</Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 }
@@ -49,46 +83,52 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f4f4f4',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  text: {
-    marginTop: 25,
-    marginBottom: 15,
+  title: {
     fontSize: 25,
     fontWeight: 'bold',
+    marginBottom: 20,
   },
   input: {
     backgroundColor: '#fff',
     borderWidth: 1,
-    borderColor: '#DDD',
+    borderColor: '#ccc',
     borderRadius: 5,
     width: '90%',
     padding: 10,
     fontSize: 18,
   },
   buttonArea: {
-    alignItems: 'center',
     flexDirection: 'row',
     marginTop: 15,
     justifyContent: 'space-around',
   },
   button: {
-    height: 55,
+    height: 50,
+    width: 150,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 15,
-    borderRadius: 5,
+    borderRadius: 10,
+  },
+  searchButton: {
+    backgroundColor: '#1d75cd',
+  },
+  clearButton: {
+    backgroundColor: '#cd5e1d',
   },
   buttonText: {
-    fontSize: 22,
+    fontSize: 18,
     color: '#fff',
   },
   result: {
-    flex: 1,
-    justifyContent: 'center',
+    marginTop: 20,
     alignItems: 'center',
   },
-  itemText: {
-    fontSize: 22,
+  resultText: {
+    fontSize: 18,
   },
 });
 
